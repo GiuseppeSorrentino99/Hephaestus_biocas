f9caa97894925c2d79714c25b84cd6063d2c4bcd
mar-ven <marcovenere99@outlook.com>
2023-06-04 10:45:48 +0200

    avgtime added

---
+++ AVGtime.py
@@ -0,0 +1,9 @@
+from pandas import read_csv
+import argparse
+
+parser = argparse.ArgumentParser(description='Software to compute mean and std from file')
+parser.add_argument("-f", "--filename", nargs='?', help='Name of the file to read')
+args = parser.parse_args()
+df = read_csv(args.filename)
+data = df.values
+print("mean time:", df['Time'].mean(), "std time:", df['Time'].std())
